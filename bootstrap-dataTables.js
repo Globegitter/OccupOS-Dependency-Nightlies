5b220b752e8f442ccf31a185ee3cf61a9752bb24
Globegitter <markus.padourek@gmail.com>
2013-04-10 17:23:39 +0100

    Updated jQuery and bootstrap-datatables.js

--- bootstrap-dataTables.js
+++ bootstrap-dataTables.js
@@ -1,6 +1,6 @@
 /* Set the defaults for DataTables initialisation */
 $.extend( true, $.fn.dataTable.defaults, {
-	"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+	"sDom": "<'row'<'col-span-6'l><'col-span-6'f>r>t<'row'<'col-span-6'i><'col-span-6'p>>",
 	"sPaginationType": "bootstrap",
 	"oLanguage": {
 		"sLengthMenu": "_MENU_ records per page"
@@ -150,7 +150,7 @@ if ( $.fn.DataTable.TableTools ) {
 /* Table initialisation example:
 $(document).ready(function() {
 	$('#example').dataTable( {
-		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+		"sDom": "<'row'<'col-span-6'l><'col-span-6'f>r>t<'row'<'col-span-6'i><'col-span-6'p>>",
 		"sPaginationType": "bootstrap",
 		"oLanguage": {
 			"sLengthMenu": "_MENU_ records per page"
